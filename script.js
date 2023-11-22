b0e3045711507c74ebdc541f356424a803613280
Tristan I <tristan04isaac@gmail.com>
2023-11-22 12:31:46 -0500

    planning steps

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log("You clicked on the button");
+  //promt user for password criteria: length 0<128, lowercase, uppercase, special characters.
+  //validate imput 
+  //generate passsword using criteria
+
+  
+  //display generated password.
+  return"Generated password";
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
