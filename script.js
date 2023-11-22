402b2a8578717bf0448a54a67c5593f3459c3ed5
Tristan I <tristan04isaac@gmail.com>
2023-11-22 12:44:50 -0500

    Password criteria prompts

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   console.log("You clicked on the button");
-  //promt user for password criteria: length 0<128, lowercase, uppercase, special characters.
+  //promt user for password criteria: length 8<128, lowercase, uppercase, special characters.
+  var confirmNumber= confirm ("Please choose a character length between 8 and 128 for your password.")
+  var confirmUppercase= confirm ("Would you like uppercase values in your password?")
+  var confirmLowercase= confirm ("Would you like lowercase values in your password?")
+  var confirmSpecial= confirm ("Would you like to add special characters in your password?")
   //validate imput 
   //generate passsword using criteria
 
